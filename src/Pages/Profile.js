4eb44995f592e3246e90e703c06799bd61d14e64
Cosmin <ghirisancosmin@gmail.com>
2022-01-21 17:32:21 +0200

    7. Profile page - minimal styling

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,18 +1,39 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components';
-import CommingSoon from '../subComponents/CommingSoon';
 import { useUserAuth } from '../userAuthContext';
+
+import * as palette from '../Variables';
+
 
 const Container = styled.div`
    height: 90%;
    display: flex;
    flex-direction: column;
    align-items: center;
-   justify-content: start;
+   justify-content: center;
    overflow: scroll;
 
    h1{
       margin: 3rem 0;
+   }
+`;
+
+const Logout = styled.button`
+   background-color: ${palette.LIGHT_COLOR};
+   color: ${palette.GREEN_BG};
+   border: 1px solid ${palette.GREEN_BG};
+   border-radius: 3px;
+   padding: 10px 20px;
+   font-size: 16px;
+   font-weight: bold;
+   letter-spacing: .5px;
+   text-decoration: none;
+   text-transform: uppercase;
+
+   :hover{
+      background-color: ${palette.GREEN_BG};
+      color: ${palette.LIGHT_COLOR};
+      cursor: pointer;
    }
 `;
 
@@ -22,8 +43,7 @@ const Profile = () => {
    return (
       <Container>
          <h1>{user && user.displayName}</h1>
-         <button onClick={signUserOut}>Log Out</button>
-         <CommingSoon />
+         <Logout onClick={signUserOut}>Log Out</Logout>
       </Container>
    )
 }
